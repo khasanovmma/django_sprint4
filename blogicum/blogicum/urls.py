4771bcab74879a34341150732609f64b2b2530eb
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-02-17 20:19:31 +0500

    feat: add django auth urls

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
     path("pages/", include("pages.urls")),
+    path("auth/", include("django.contrib.auth.urls")),
 ]
 
 handler404 = "core.views.page_not_found"
