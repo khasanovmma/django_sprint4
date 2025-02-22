ea87d85d9ea3943804447adec7387dce1ec3ff57
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-02-22 21:55:42 +0500

    refact: update blog urlpatterns

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,30 +1,30 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 app_name = "blog"
 
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("posts/<int:post_id>/", views.post_detail, name="post_detail"),
-    path("posts/create/", views.create_post, name="create_post"),
-    path("posts/<int:post_id>/edit/", views.edit_post, name="edit_post"),
-    path("posts/<int:post_id>/delete/", views.delete_post, name="delete_post"),
+post_urls = [
+    path("<int:post_id>/", views.post_detail, name="post_detail"),
+    path("create/", views.create_post, name="create_post"),
+    path("<int:post_id>/edit/", views.edit_post, name="edit_post"),
+    path("<int:post_id>/delete/", views.delete_post, name="delete_post"),
+    path("<int:post_id>/comment/", views.add_comment, name="add_comment"),
     path(
-        "posts/<int:post_id>/comment/",
-        views.add_comment,
-        name="add_comment",
-    ),
-    path(
-        "posts/<int:post_id>/edit_comment/<int:comment_id>/",
+        "<int:post_id>/edit_comment/<int:comment_id>/",
         views.edit_comment,
         name="edit_comment",
     ),
     path(
-        "posts/<int:post_id>/delete_comment/<int:comment_id>/",
+        "<int:post_id>/delete_comment/<int:comment_id>/",
         views.delete_comment,
         name="delete_comment",
     ),
+]
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("posts/", include(post_urls)),
     path(
         "category/<slug:category_slug>/",
         views.category,
