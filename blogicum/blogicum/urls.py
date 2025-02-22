58be41c56b07e6b184693cafc31f3ecce7e8a8d2
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-02-22 19:45:52 +0500

    refact: edit user data with django auth user create form

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, reverse_lazy
 from django.views.generic.edit import CreateView
-
-from blog.forms import CreateProfileForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 urlpatterns = [
@@ -16,7 +15,7 @@ urlpatterns = [
         "auth/registration/",
         CreateView.as_view(
             template_name="registration/registration_form.html",
-            form_class=CreateProfileForm,
+            form_class=UserCreationForm,
             success_url=reverse_lazy("blog:index"),
         ),
         name="registration",
